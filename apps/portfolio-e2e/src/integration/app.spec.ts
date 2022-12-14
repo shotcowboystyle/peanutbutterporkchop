d7a65b68de006e219d4dbc72e46133d027e05a84
Curtis Blanton <curt.blanton@gmail.com>
2022-12-14 12:13:35 -0500

    feat: Added boilerplate for Nuxt portfolio app

---
+++ apps/portfolio-e2e/src/integration/app.spec.ts
@@ -0,0 +1,6 @@
+describe('portfolio', () => {
+  it('should display welcome message', () => {
+    cy.visit('/');
+    cy.contains('h2', 'Welcome to your Nuxt Application');
+  });
+});
---
+++ apps/portfolio/nuxt.config.ts
@@ -0,0 +1,114 @@
+import UnpluginComponentsVite from 'unplugin-vue-components/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default {
+  // server side rendering mode
+  ssr: true,
+
+  // typescripts
+  typescript: {
+    strict: true,
+    typeCheck: true,
+  },
+
+  // css
+  css: [
+    'virtual:windi-base.css',
+    'virtual:windi-components.css',
+    'virtual:windi-utilities.css',
+    '~/assets/sass/vendor.scss',
+    '~/assets/sass/app.scss',
+  ],
+
+  // plugins
+  plugins: ['~/plugins/navbar.ts'],
+
+  // build
+  build: {
+    transpile: ['@headlessui/vue'],
+  },
+
+  buildDir: '../../dist/portfolio/.nuxt',
+
+  // modules
+  modules: [
+    'unplugin-icons/nuxt',
+    '@intlify/nuxt3',
+    '@pinia/nuxt',
+    '@nuxt/content',
+    '@vueuse/nuxt',
+    'nuxt-windicss',
+  ],
+
+  modulesDir: ['../../node_modules'],
+
+  // experimental features
+  experimental: {
+    reactivityTransform: false,
+  },
+
+  // auto import components
+  components: true,
+
+  // vite plugins
+  vite: {
+    plugins: [
+      UnpluginComponentsVite({
+        dts: true,
+        resolvers: [
+          IconsResolver({
+            prefix: 'Icon',
+          }),
+        ],
+      }),
+    ],
+  },
+
+  // app config
+  app: {
+    // global transition
+    pageTransition: { name: 'page', mode: 'out-in' },
+    layoutTransition: { name: 'layout', mode: 'out-in' },
+  },
+
+  // localization - i18n config
+  intlify: {
+    localeDir: 'locales',
+    vueI18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      availableLocales: ['en', 'id', 'ja', 'ko'],
+    },
+  },
+
+  // vueuse
+  vueuse: {
+    ssrHandlers: true,
+  },
+
+  // windicss
+  windicss: {
+    analyze: {
+      analysis: {
+        interpretUtilities: false,
+      },
+      server: {
+        port: 4000,
+        open: false,
+      },
+    },
+    scan: true,
+  },
+
+  // content
+  content: {
+    documentDriven: true,
+    markdown: {
+      mdc: true,
+    },
+    highlight: {
+      theme: 'github-dark',
+    },
+  },
+};
